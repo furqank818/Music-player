aa34ab25308ae6ea1207b5075900189b8892370e
furqan khan <khanfurqan818@gmail.com>
2021-09-19 21:39:00 +0530

    complete playlist section

---
+++ app.js
@@ -0,0 +1,155 @@
+
+let currentMusic = 0;
+
+const music = document.querySelector('#audio');
+const seekBar = document.querySelector('.seek-bar');
+const songName = document.querySelector(".music-name");
+const artistName = document.querySelector(".artist-name");
+const disk = document.querySelector(".disk");
+const currentTime = document.querySelector(".current-time");
+const musicDuration = document.querySelector(".song-duration");
+const playBtn = document.querySelector(".play-btn");
+const forwardBtn = document.querySelector(".forward-btn");
+const backwardBtn = document.querySelector(".backward-btn");
+const playListBtn = document.querySelector("#playlist");
+const musicList = document.querySelector(".music-list");
+const udL = document.querySelector("ul");
+
+
+
+
+//set a music 
+const setMusic = (i) => {
+    seekBar.value = 0;
+    let song = songs[i];
+    currentMusic = i;
+    music.src = song.path;
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    disk.style.backgroundImage = `url('${song.cover}')`;
+    currentTime.innerHTML = "00:00";
+    music.addEventListener('loadeddata', () => {
+        seekBar.max = music.duration;
+        musicDuration.innerHTML = formatTime(music.duration);
+  })
+}
+
+
+
+//formatting the time
+const formatTime = (time)=>{
+    let min = Math.floor(time / 60);
+    if(min < 10){
+        min = `0${min}`;
+    }
+    let sec = Math.floor(time % 60);
+    if(sec < 10){
+        sec = `0${sec}`;
+    }
+    return `${min}:${sec}`;
+}
+// playbutton
+playBtn.addEventListener('click',()=>{
+    if(playBtn.className.includes('pause')){
+        music.play();
+        
+    }else{
+        music.pause();
+    }
+    playBtn.classList.toggle("pause");
+    disk.classList.toggle("play");
+})
+
+//seekbar
+
+setInterval(()=>{
+    seekBar.value = music.currentTime;
+    currentTime.innerHTML=formatTime(music.currentTime)
+},500)
+
+seekBar.addEventListener('change',()=>{
+    music.currentTime = seekBar.value;
+})
+// forwardbutton
+forwardBtn.addEventListener("click",() => {
+    if(currentMusic >= songs.length -1){
+        currentMusic = 0;
+    }else{
+        currentMusic++;
+    }
+    setMusic(currentMusic);
+    playMusic();
+})
+// backwardbutton
+backwardBtn.addEventListener("click",()=>{
+    if(currentMusic <= 0){
+        currentMusic = songs.length -1;
+    }else{
+        currentMusic--;
+    }
+    setMusic(currentMusic);
+    playMusic();
+})
+//function to play song
+const playMusic = () =>{
+    music.play();
+    playBtn.classList.remove("pause");
+    disk.classList.add("play");
+}
+setInterval(() => {
+    seekBar.value = music.currentTime;
+    currentTime.innerHTML = formatTime(music.currentTime);
+    if(Math.floor(music.currentTime) == Math.floor(seekBar.max)){
+        if(music.loop == true){
+            music.currentTime = 0;
+        }else{
+        forwardBtn.click();
+        }
+    }
+}, 500)
+//function to loop song
+let repIcon = document.querySelector('#loopbutton')
+    function handleRepeat() {
+        console.log(music.loop);
+        if (music.loop == true) {
+            music.loop = false;
+            repIcon.classList.toggle('active');
+        }
+        else {
+            music.loop = true;
+            repIcon.classList.toggle('active');
+            
+            
+        }
+    }
+// function to set volume 
+function thisVolume(volume_value)
+    {
+        music.volume = volume_value / 100;
+    }
+
+
+// playlist button
+playListBtn.addEventListener("click",()=>{
+    musicList.classList.toggle("show");
+    playListBtn.classList.toggle("active");
+    
+});
+
+
+
+//songs will be displayed in the playlist
+for(let i = 0;i < songs.length;i++){
+    
+    let listTag = `<li  class="list" onclick="clicked(${i})">${songs[i].name}<span>${" - "+songs[i].artist}</span></li>`;
+    udL.insertAdjacentHTML("beforeend",listTag);
+}
+// song will be played when clicked from the playlist
+function clicked(songNo){
+    
+    let musicIndex = songNo; //updating current song index with clicked li index
+    setMusic(musicIndex);
+    playMusic();
+  }
+
+
---
+++ data.js
@@ -0,0 +1,44 @@
+let songs = [
+    {
+        name: 'See You Again',
+        path: 'musics/seeyou.mp3',
+        artist: 'WizKhalifa ft.Charlie Puth',
+        cover: 'images/cover/seeyou.jpg'
+    },
+    {
+        name: 'sing me to sleep',
+        path: 'musics/sing.mp3',
+        artist: 'Alan walker',
+        cover: 'images/cover/sing.jpg'
+    },
+    {
+        name: 'force',
+        path: 'musics/force.mp3',
+        artist: 'Alanwalker',
+        cover: 'images/cover/force.jpg'
+    },
+    {
+        name: 'Different world',
+        path: 'musics/different_world.mp3',
+        artist: 'Alan walker',
+        cover: 'images/cover/different.png'
+    },
+    {
+        name: 'Faded',
+        path: 'musics/faded.mp3',
+        artist: 'Alan walker',
+        cover: 'images/cover/faded.jpg'
+    },
+    {
+        name: 'Bad Liar',
+        path: 'musics/Badliar.mp3',
+        artist: 'imagine dragrons',
+        cover: 'images/cover/badliar.jpg'
+    },
+    {
+        name: 'Believer',
+        path: 'musics/believer.mp3',
+        artist: 'imagine dragons',
+        cover: 'images/cover/bilever.jpg'
+    },
+]
